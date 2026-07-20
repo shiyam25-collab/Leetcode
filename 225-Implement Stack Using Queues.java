a528a83d93526d3f1ad502f30463425c2a0a3383
shiyam25-collab <shiyamluffy25@gmail.com>
2026-07-20 18:25:29 +0530

    225-Implement Stack Using Queues.java
    Implemented MyStack class using a queue to simulate stack operations. The class supports push, pop, top, and empty methods.

---
+++ 225-Implement Stack Using Queues.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    // Pushes element x to the top of the stack.
+    public void push(int x) {
+        // Get the current size before adding the new element
+        int size = queue.size();
+        
+        // Add the new element to the back of the queue
+        queue.add(x);
+        
+        // Rotate the queue to bring the new element to the front
+        for (int i = 0; i < size; i++) {
+            queue.add(queue.remove());
+        }
+    }
+    
+    // Removes the element on the top of the stack and returns it.
+    public int pop() {
+        return queue.remove();
+    }
+    
+    // Returns the element on the top of the stack.
+    public int top() {
+        return queue.peek();
+    }
+    
+    // Returns true if the stack is empty, false otherwise.
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
